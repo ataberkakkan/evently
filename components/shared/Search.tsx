bb9a5ecaae52a1f97ca26435c5fc122c9a84c3fb
ataberkakkan <akkanataberk@gmail.com>
2024-01-11 10:39:23 +0300

    Search Functionality

---
+++ components/shared/Search.tsx
@@ -0,0 +1,65 @@
+"use client";
+
+import Image from "next/image";
+import { useEffect, useState } from "react";
+import { Input } from "../ui/input";
+import { formUrlQuery, removeKeysFromQuery } from "@/lib/utils";
+import { useSearchParams } from "next/navigation";
+import { useRouter } from "next/navigation";
+
+const Search = ({
+  placeholder = "Search title...",
+}: {
+  placeholder?: string;
+}) => {
+  const [query, setQuery] = useState("");
+
+  const router = useRouter();
+  const searcParams = useSearchParams();
+
+  useEffect(() => {
+    const delayDebounceFn = setTimeout(() => {
+      let newUrl = "";
+
+      if (query) {
+        newUrl = formUrlQuery({
+          params: searcParams.toString(),
+          key: "query",
+          value: query,
+        });
+      } else {
+        newUrl = removeKeysFromQuery({
+          params: searcParams.toString(),
+          keysToRemove: ["query"],
+        });
+      }
+
+      router.push(newUrl, { scroll: false });
+    }, 3000);
+
+    return () => clearTimeout(delayDebounceFn);
+  }, [query, searcParams, router]);
+
+  return (
+    <div
+      className="flex-center min-h-[44px] w-full overflow-hidden
+    rounded-full bg-grey-50 px-4 py-2"
+    >
+      <Image
+        src="/assets/icons/search.svg"
+        alt="search"
+        width={24}
+        height={24}
+      />
+      <Input
+        type="text"
+        placeholder={placeholder}
+        onChange={(e) => setQuery(e.target.value)}
+        className="p-regular-16 border-0 bg-grey-50 outline-offset-0
+            placeholder:text-grey-500 focus:border-0 focus-visible:ring-0
+            focus-visible:ring-offset-0"
+      />
+    </div>
+  );
+};
+export default Search;
