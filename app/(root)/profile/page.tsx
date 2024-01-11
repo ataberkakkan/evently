48bf0e3fad9598ee04979454a865165a69e577f0
ataberkakkan <akkanataberk@gmail.com>
2024-01-11 11:27:03 +0300

    Last touches

--- app/(root)/profile/page.tsx
+++ app/(root)/profile/page.tsx
@@ -1,19 +1,22 @@
 import Collection from "@/components/shared/Collection";
 import { Button } from "@/components/ui/button";
 import { getEventsByUser } from "@/lib/actions/event.actions";
+import { SearchParamProps } from "@/types";
 import { auth } from "@clerk/nextjs";
 import Link from "next/link";
 
-const ProfilePage = async () => {
+const ProfilePage = async ({ searchParams }: SearchParamProps) => {
   const { sessionClaims } = auth();
 
   const userId = sessionClaims?.userId as string;
+
+  const eventsPage = Number(searchParams?.eventsPage) || 1;
 
   const organizedEvents = await getEventsByUser({ userId, page: 1 });
 
   return (
     <>
-      <section className="bg-primary-50 bg-dotted-pattern bg-cover bg-center py-5 md:py-10">
+      {/* <section className="bg-primary-50 bg-dotted-pattern bg-cover bg-center py-5 md:py-10">
         <div className="wrapper flex items-center justify-center sm:justify-between">
           <h3 className="h3-bold text-center sm:text-left">My Tickets</h3>
           <Button asChild size="lg" className="button hidden sm:flex">
@@ -33,29 +36,29 @@ const ProfilePage = async () => {
           urlParamName="ordersPage"
           totalPages={2}
         />
-      </section>
+      </section> */}
 
-      {/* <section className="bg-primary-50 bg-dotted-pattern bg-cover bg-center py-5 md:py-10">
+      <section className="bg-primary-50 bg-dotted-pattern bg-cover bg-center py-5 md:py-10">
         <div className="wrapper flex items-center justify-center sm:justify-between">
           <h3 className="h3-bold text-center sm:text-left">Events Organized</h3>
           <Button asChild size="lg" className="button hidden sm:flex">
-            <Link href="/#events">Create New Event</Link>
+            <Link href="/events/create">Create New Event</Link>
           </Button>
         </div>
       </section>
 
       <section className="wrapper my-8">
         <Collection
-          data={events?.data}
+          data={organizedEvents?.data}
           emptyTitle="No events have been created yet."
           emptyStateSubtext="Go create some now"
           collectionType="My_Tickets"
           limit={6}
-          page={1}
+          page={eventsPage}
           urlParamName="eventsPage"
-          totalPages={2}
+          totalPages={organizedEvents?.totalPages}
         />
-      </section> */}
+      </section>
     </>
   );
 };
